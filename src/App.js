342907d7fd60751343836c48e0da88c8512e67fd
GeunTaekOh <rmsxor94@naver.com>
2021-04-10 16:41:44 +0900

    [퍼포먼스 개선]
    todos 값이 바뀔때마다 onRemove, onToglle 도 새롭게 바뀌지 않도록

--- src/App.js
+++ src/App.js
@@ -40,39 +40,63 @@ const App = () => {
 
   const [todos, setTodos] = useState(createBulkTodos); // 여기에 함수 이름만 적었는데, 함수처럼 () 를 같이 적으면 리렌더링될때마다 이 함수가 호출됨.
 
-  const nextId = useRef(2501); // id 값은 렌더되지 않으므로 퍼포먼스를 위해 ref 로 만들기
+  const nextId = useRef(4); // id 값은 렌더되지 않으므로 퍼포먼스를 위해 ref 로 만들기
   
   //onInsert 도 리랜더링 될 때마다 새로 함수를 실행하지 않도록 useCallBack 사용
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id:nextId.current,
-        text,
-        checked:false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos],
-  );
+  // const onInsert = useCallback(
+  //   text => {
+  //     const todo = {
+  //       id:nextId.current,
+  //       text,
+  //       checked:false,
+  //     };
+  //     setTodos(todos.concat(todo));
+  //     nextId.current += 1;
+  //   },
+  //   [todos],
+  // );
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked : false,
+    };
+    setTodos(todos => todos.concat(todo));
+    nextId.current += 1;
+  }, []);    // 위 방식과 다르게 useState의 함수형 업데이트 -> todos 배열이 업데이트되면 이 함수도 같이 바뀌지 않음.
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    },
-    [todos],
-  );
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo => 
-          todo.id === id ? {...todo, checked: !todo.checked} : todo,),
+  // const onRemove = useCallback(
+  //   id => {
+  //     setTodos(todos.filter(todo => todo.id !== id));
+  //   },
+  //   [todos],
+  // );
+  const onRemove = useCallback(id => {
+    setTodos(toidos => todos.filter(todo => todo.id !== id));
+  },[]);  // 위 방식과 다르게 useState의 함수형 업데이트 -> todos 배열이 업데이트되면 이 함수도 같이 바뀌지 않음.
+
+  
+  // const onToggle = useCallback(
+  //   id => {
+  //     setTodos(
+  //       todos.map(todo => 
+  //         todo.id === id ? {...todo, checked: !todo.checked} : todo,),
+  //     );
+  //   },
+  //   [todos],
+  // );
+
+  const onToggle = useCallback(id => {
+    setTodos( todos => 
+      todos.map(todo =>
+        todo.id === id ? {...todo, checked: !todo.checked} : todo,
+        ),
       );
-    },
-    [todos],
-  );
+  },[]);
 
+
+  //todos 배열이 바뀌게 되면 onRemove 와 onToggle 함수도 새로 바뀜.
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert}/>
